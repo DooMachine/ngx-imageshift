780d95c7b001e8fbabb4901baf5ad29c990022f3
DooMachine <okanaslankan@hotmail.com>
2018-08-31 23:11:14 +0300

    test test

--- src/directives/image-shift.directive.ts
+++ src/directives/image-shift.directive.ts
@@ -1,8 +1,10 @@
 import { Directive, Input, OnDestroy, AfterViewInit, HostListener, Output, EventEmitter, ElementRef, Renderer2 } from '@angular/core';
 import { ImageInfo } from '../models/imageinfo';
-import { Subject, Observable, of, interval } from 'rxjs';
-import { debounceTime ,takeUntil } from 'rxjs/operators';
+// tslint:disable-next-line:import-blacklist
+import { Subject, Observable, interval } from 'rxjs';
+import { debounceTime , takeUntil } from 'rxjs/operators';
 @Directive({
+    // tslint:disable-next-line:directive-selector
     selector: '[imageShift]'
 })
 export class ImageShiftDirective implements OnDestroy, AfterViewInit {
@@ -10,8 +12,7 @@ export class ImageShiftDirective implements OnDestroy, AfterViewInit {
     private shiftStartNotifier: Subject<any> = new Subject();
     private shiftEndNotifier: Subject<any> = new Subject();
 
-    constructor(private el: ElementRef, private renderer: Renderer2)
-    {
+    constructor(private el: ElementRef, private renderer: Renderer2) {
         this.registerShiftEvents();
     }
     // Array of ImageInfo or Image Urls
@@ -19,30 +20,28 @@ export class ImageShiftDirective implements OnDestroy, AfterViewInit {
     // Next Index of image that will preloaded
     private preloadImageIndex: number;
     // Preloaded (cached) Image Indexs
-    private preloadedImageIndexes : Array<number> = new Array<number>();
+    private preloadedImageIndexes: Array<number> = new Array<number>();
     // Currently shown image Index
-    private _activeImageIndex: number = 0;
+    private _activeImageIndex = 0;
     // Default Image Index in case of hover
-    private _defaultImageIndex: number = 0;
+    private _defaultImageIndex = 0;
     // Period of image shifting in ms
-    private _period: number = 300;
-    private _shiftOnHover: boolean = false;
+    private _period = 300;
+    private _shiftOnHover = false;
     private _interval: Observable<number>;
 
-    @Input() public preloadImages: boolean= true;
-    @Input() public loop: boolean = false;
-    @Output() public onImageChange: EventEmitter<ImageInfo | string> = new EventEmitter<ImageInfo | string>();
-   
-    
+    @Input() public preloadImages = true;
+    @Input() public loop = false;
+    @Output() public imageChanged: EventEmitter<ImageInfo | string> = new EventEmitter<ImageInfo | string>();
+
 
     @Input('images')
     set images(images: Array<ImageInfo | string>) {
-        if(!Array.isArray(images))
-        {
-            throw new Error("images should be instance of ImageInfo or string array");
+        if (!Array.isArray(images)) {
+            throw new Error('images should be instance of ImageInfo or string array');
         }
         this._images = images;
-        this.preloadImageIndex = (this.defaultImageIndex + 1) % (this._images.length-1);
+        this.preloadImageIndex = (this.defaultImageIndex + 1) % (this._images.length - 1);
     }
     get images(): Array<ImageInfo | string> {
         return this._images;
@@ -50,7 +49,7 @@ export class ImageShiftDirective implements OnDestroy, AfterViewInit {
 
     @Input('defaultImageIndex')
     set defaultImageIndex(ind: number) {
-        this._defaultImageIndex = ind % (this._images.length-1);
+        this._defaultImageIndex = ind % (this._images.length - 1);
     }
     get defaultImageIndex(): number {
         return this._defaultImageIndex;
@@ -60,7 +59,6 @@ export class ImageShiftDirective implements OnDestroy, AfterViewInit {
         this._period = value;
         this._interval = interval(value);
     }
-    
     get period(): number {
         return this._period;
     }
@@ -73,144 +71,113 @@ export class ImageShiftDirective implements OnDestroy, AfterViewInit {
         return this._shiftOnHover;
     }
 
-    @HostListener('mouseenter') onMouseEnter()
-    {
-        if (this.shiftOnHover)
-        {
+    @HostListener('mouseenter') onMouseEnter() {
+        if (this.shiftOnHover) {
             this.shiftStartNotifier.next();
-        }        
+        }
     }
-    
-    @HostListener('mouseleave') onMouseLeave()
-    {
-        if (this.shiftOnHover)
-        {
+
+    @HostListener('mouseleave') onMouseLeave() {
+        if (this.shiftOnHover) {
             this.shiftEndNotifier.next();
         }
     }
 
-    ngAfterViewInit() : void {
+    ngAfterViewInit(): void {
         this.preLoadNextImage();
         this.assignAttrsIfNotSet();
-        if (!this.shiftOnHover)
-        {
+        if (!this.shiftOnHover) {
             this.shiftStartNotifier.next();
         }
-        if (this.preloadImages)
-        {
+        if (this.preloadImages) {
             this.loadAllImages();
         }
     }
     ngOnDestroy(): void {
         this.shiftStartNotifier.unsubscribe();
-        this.shiftEndNotifier.unsubscribe();        
+        this.shiftEndNotifier.unsubscribe();
     }
 
-    private registerShiftEvents() : void
-    {
+    private registerShiftEvents(): void {
         this.shiftStartNotifier
-            .subscribe(e=>{
-                this._interval.pipe(takeUntil(this.shiftEndNotifier)).subscribe((ind)=>{
-                    if (!this.loop && this._activeImageIndex == this.images.length-1)
-                    {
+            .subscribe(e => {
+                this._interval.pipe(takeUntil(this.shiftEndNotifier)).subscribe((ind) => {
+                    if (!this.loop && this._activeImageIndex === this.images.length - 1) {
                         this.shiftEndNotifier.next();
-                    }
-                    else
-                    {
-                        this._activeImageIndex = (this._activeImageIndex+1) % (this.images.length);
+                    } else {
+                        this._activeImageIndex = (this._activeImageIndex + 1) % (this.images.length);
                         const imageInfo: any = this._images[this._activeImageIndex];
-                        if (imageInfo.hasOwnProperty('src'))
-                        {
-                            this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo['src'])
-                            this.renderer.setAttribute(this.el.nativeElement,'alt',imageInfo['alt'])
+                        if (imageInfo.hasOwnProperty('src')) {
+                            this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo['src']);
+                            this.renderer.setAttribute(this.el.nativeElement, 'alt', imageInfo['alt']);
+                        } else {
+                            this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo as string);
                         }
-                        else
-                        {
-                            this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo as string)
-                        }
-                        this.onImageChange.emit(imageInfo);
-                        this.preloadImageIndex = (this._activeImageIndex+1) % (this.images.length);
-                        this.preLoadNextImage()
+                        this.imageChanged.emit(imageInfo);
+                        this.preloadImageIndex = (this._activeImageIndex + 1) % (this.images.length);
+                        this.preLoadNextImage();
                     }
-                })
+                });
             });
         this.shiftEndNotifier.pipe(debounceTime(this._period))
-            .subscribe(e=>{
-                if (!this.loop && this._activeImageIndex == this.images.length-1)
-                {
+            .subscribe(e => {
+                if (!this.loop && this._activeImageIndex === this.images.length - 1) {
                     return;
                 }
                 this._activeImageIndex = this._defaultImageIndex;
                 const imageInfo: any = this._images[this._activeImageIndex];
-                if (imageInfo.hasOwnProperty('src'))
-                    {
-                        this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo['src'])
-                        this.renderer.setAttribute(this.el.nativeElement,'alt',imageInfo['alt'])
+                if (imageInfo.hasOwnProperty('src')) {
+                        this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo['src']);
+                        this.renderer.setAttribute(this.el.nativeElement, 'alt', imageInfo['alt']);
+                    } else {
+                        this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo as string);
                     }
-                    else
-                    {
-                        this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo as string)
-                    }
-                    this.onImageChange.emit(imageInfo);
+                    this.imageChanged.emit(imageInfo);
             });
     }
 
-    private preLoadNextImage()
-    {
-        if (this.preloadedImageIndexes.findIndex(p => p == this.preloadImageIndex) === -1)
-        {
+    private preLoadNextImage() {
+        if (this.preloadedImageIndexes.findIndex(p => p === this.preloadImageIndex) === -1) {
             const pre_image = new Image();
             const imageInfo: any = this._images[this.preloadImageIndex];
-            if (imageInfo.hasOwnProperty('src'))
-            {
+            if (imageInfo.hasOwnProperty('src')) {
                 pre_image.src = imageInfo['src'];
-            }
-            else
-            {
+            } else {
                 pre_image.src = imageInfo as string;
             }
             this.preloadedImageIndexes.push(this.preloadImageIndex);
-            this.preloadImageIndex = (this.preloadImageIndex + 1) % (this._images.length-1);
+            this.preloadImageIndex = (this.preloadImageIndex + 1) % (this._images.length - 1);
         }
     }
-    private loadAllImages()
-    {
+    private loadAllImages() {
         for (let i = 0; i < this.images.length; i++) {
-            const imageInfo : any = this.images[i];
-            this.loadImage(imageInfo).then(()=>{
+            const imageInfo: any = this.images[i];
+            this.loadImage(imageInfo).then(() => {
                 this.preloadedImageIndexes.push(i);
-                this.preloadImageIndex = (i + 1) % (this.images.length-1);
-            })
+                this.preloadImageIndex = (i + 1) % (this.images.length - 1);
+            });
         }
     }
-    private async loadImage(imageInfo: any)
-    {
+    private async loadImage(imageInfo: any) {
         const pre_image = new Image();
-        if (imageInfo.hasOwnProperty('src'))
-        {
+        if (imageInfo.hasOwnProperty('src')) {
             pre_image.src = imageInfo['src'];
-        }
-        else
-        {
+        } else {
             pre_image.src = imageInfo as string;
         }
     }
-    private assignAttrsIfNotSet()
-    {
+    private assignAttrsIfNotSet() {
         const src = this.el.nativeElement.getAttribute('src');
-        if (!src)
-        {
+        if (!src) {
             const imageInfo: any = this.images[this.defaultImageIndex];
-            if (!imageInfo)
+            if (!imageInfo) {
                 return;
-            if (imageInfo.hasOwnProperty('src'))
-            {
-                this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo['src'])
-                this.renderer.setAttribute(this.el.nativeElement,'alt',imageInfo['alt'])
             }
-            else
-            {
-                this.renderer.setAttribute(this.el.nativeElement,'src',imageInfo as string)
+            if (imageInfo.hasOwnProperty('src')) {
+                this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo['src']);
+                this.renderer.setAttribute(this.el.nativeElement, 'alt', imageInfo['alt']);
+            } else {
+                this.renderer.setAttribute(this.el.nativeElement, 'src', imageInfo as string);
             }
         }
     }
--- tests/image-shift.directive.spec.ts
+++ tests/image-shift.directive.spec.ts
@@ -2,29 +2,29 @@ import {
     Component, DebugElement
 } from '@angular/core';
 import {TestBed, ComponentFixture} from '@angular/core/testing';
-import {By} from "@angular/platform-browser";
-import { ImageInfo, ImageShiftDirective } from 'public_api';
+import { By } from '@angular/platform-browser';
+import { ImageInfo } from '../src/models/imageinfo';
+import { ImageShiftDirective } from '../src/directives/image-shift.directive';
 
 @Component({
-    template: `<img [src]="imageInfos[0].src" alt="imageInfos[0].alt" 
+    template: `<img [src]="imageInfos[0].src" alt="imageInfos[0].alt"
                     imageShift
                     [images] = "imageInfos"
                     [period] = "1000"
                     [shiftOnHover]="true"
                     [preloadImages]="true"
-                />` 
+                />`
   })
   class TestShiftComponent {
-    imageInfos : Array<ImageInfo> = new Array<ImageInfo>();
-    imageInfostring : Array<string> = new Array<string>();
-    constructor()
-    {
+    imageInfos: Array<ImageInfo> = new Array<ImageInfo>();
+    imageInfostring: Array<string> = new Array<string>();
+    constructor() {
         for (let i = 0; i < 12; i++) {
             this.imageInfos.push({
-              src: 'https://placeimg.com/'+(400+10*i).toString()+'/480/any',
-              alt:i.toString()
-            });  
-            this.imageInfostring.push('https://placeimg.com/'+(100+50*i).toString()+'/200/any');   
+              src: 'https://placeimg.com/' + (400 + 10 * i).toString() + '/480/any',
+              alt: i.toString()
+            });
+            this.imageInfostring.push('https://placeimg.com/' + (100 + 50 * i).toString() + '/200/any');
           }
     }
   }
@@ -34,12 +34,12 @@ import { ImageInfo, ImageShiftDirective } from 'public_api';
     let component: TestShiftComponent;
     let fixture: ComponentFixture<TestShiftComponent>;
     let inputEl: DebugElement;
-  
+
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestShiftComponent, ImageShiftDirective] 
+        declarations: [TestShiftComponent, ImageShiftDirective]
       });
-      fixture = TestBed.createComponent(TestShiftComponent); 
+      fixture = TestBed.createComponent(TestShiftComponent);
       component = fixture.componentInstance;
       inputEl = fixture.debugElement.query(By.css('img'));
     });
